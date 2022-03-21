c34c18f40869d0cb23dd927eceae7a1121954690
kopijahe <32376719+kopijahe@users.noreply.github.com>
2022-03-21 15:09:23 +0700

    Update lteipchecker-telnet.sh
    - make the codes more uniform (to space or not to space after `|` ?)
    
    - replace backticks with `$(...)` (see: [SC2006](https://github.com/koalaman/shellcheck/wiki/SC2006))

--- script/lteipchecker-telnet.sh
+++ script/lteipchecker-telnet.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 while sleep 5; do
-{ echo "nvram_get cache wan_ipaddr"; echo "nvram_get cache wan_primary_dns"; sleep 2; } | telnet 169.254.0.1|grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}'|grep -v "169.254.0.1"  > /tmp/ipmodem.txt
+{ echo "nvram_get cache wan_ipaddr"; echo "nvram_get cache wan_primary_dns"; sleep 2; } | telnet 169.254.0.1 | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -v "169.254.0.1"  > /tmp/ipmodem.txt
 
-ethdataip=`ifconfig modem.103 | grep 'inet addr' | cut -d: -f2 | awk '{print $1}'`
-lteip=`head -1 /tmp/ipmodem.txt`
-ltedns=`tail -1 /tmp/ipmodem.txt`
+ethdataip=$(ifconfig modem.103 | grep 'inet addr' | cut -d: -f2 | awk '{print $1}')
+lteip=$(head -1 /tmp/ipmodem.txt)
+ltedns=$(tail -1 /tmp/ipmodem.txt)
+
 if [ -z "$lteip" ]
 then
       echo "IP modem tidak dapat di cek"
